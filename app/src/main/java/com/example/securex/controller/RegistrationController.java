c66ee899d9f9f1d584004496ba52a4cb7e9151bb
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-03-23 16:53:31 +0530

    user model done

--- app/src/main/java/com/example/securex/controller/RegistrationController.java
+++ app/src/main/java/com/example/securex/controller/RegistrationController.java
@@ -1,4 +1,12 @@
 package com.example.securex.controller;
 
 public class RegistrationController {
+
+    public RegistrationController() {
+    }
+
+
+    public void createUser(String Username,String Email, String Color, int Pin, String Password, int PasswordSize){
+
+    }
 }
